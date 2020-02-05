bb049cc5235d822f4784f4c3ec76684dd418fefe
Stanley Sham <stanley.sham94@gmail.com>
2020-02-05 14:26:11 -0500

    added more css and figuring out modals

--- src/components/InterviewBoard/InterviewBoard.jsx
+++ src/components/InterviewBoard/InterviewBoard.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './InterviewBoard.css';
 import InterviewCard from '../InterviewCard/InterviewCard';
 
-const numQuestions = [1,2,3,4,5,6,7,8,9,10]
+const numQuestions = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 const InterviewBoard = (props) => (
     <div className = 'InterviewBoard'>
         <div className = 'title-card'>
-            <h1>Top 10 Asked Questions</h1>
+            <h1>Top 20 Asked Questions</h1>
         </div>
         <div className = 'InterviewBoard-grid'>
-            {numQuestions.map((num, idx) =>
+            {numQuestions.map(num =>
                 <InterviewCard 
                 questionNum = {num}
                 />
--- src/components/InterviewCard/InterviewCard.jsx
+++ src/components/InterviewCard/InterviewCard.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './InterviewCard.css';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+function InterviewCards() {
+    const [show, setShow] = useState(false);
+
+    const questionDict = {1: 'Tell me about yourself?', 2: 'Why do you want to work here?', 3: 'Question 3', 4: 'Question 4', 5: 'Question 5', 6: 'Question 6', 7: 'Question 7', 8: 'Question 8', 9: 'Question 9', 10: 'Question 10', 11: 'Question 11', 12: 'Question 12', 13: 'Question 13', 14: 'Question 14', 15: 'Question 15', 16: 'Question 16', 17: 'Question 17', 18: 'Question 18', 19: 'Question 19', 20: 'Question 20'}
+
+
+}
 
 const questionDict = {1: 'Tell me about yourself?', 2: 'Why do you want to work here?', 3: 'Question 3', 4: 'Question 4', 5: 'Question 5', 6: 'Question 6', 7: 'Question 7', 8: 'Question 8', 9: 'Question 9', 10: 'Question 10'}
 
 
 const InterviewCard = (props) => (
     <div className='InterviewCard'>
-        <div className='first'>
-            <Card style={{ width: '300px', height: '200px'}}>
+        <div className={(props.questionNum) + 'card'}>
+            <Card style={{ width: '300px', height: '175px'}}>
                 <Card.Body>
-                    <h1>Question:</h1>
-                    <h2>
-                        {questionDict[props.questionNum]}
-                    </h2>
+<Card.Text><h1>Question: {props.questionNum}</h1>
+                    <h3>{questionDict[props.questionNum]}</h3>
+                    </Card.Text>
                 </Card.Body>
+                <Card.Footer>
+                    <Button variant="outline-info" className="practice">Practice Now</Button>
+                </Card.Footer>
             </Card>
         </div>
     </div>
