db640ebd59d3301f3725bc0e4ee1197f43eb4113
Stanley Sham <stanley.sham94@gmail.com>
2020-02-05 23:17:21 -0500

    turned modal into grid so that it is more customizable

--- src/components/InterviewCard/InterviewCard.jsx
+++ src/components/InterviewCard/InterviewCard.jsx
@@ -8,7 +8,7 @@ import QuestionModal from '../QuestionModal/QuestionModal'
 function InterviewCard(props) {
     const [show, setShow] = useState(false);
 
-    const questionDict = {1: 'Tell me about yourself?', 2: 'Why do you want to work here?', 3: 'Question 3', 4: 'Question 4', 5: 'Question 5', 6: 'Question 6', 7: 'Question 7', 8: 'Question 8', 9: 'Question 9', 10: 'Question 10', 11: 'Question 11', 12: 'Question 12', 13: 'Question 13', 14: 'Question 14', 15: 'Question 15', 16: 'Question 16', 17: 'Question 17', 18: 'Question 18', 19: 'Question 19', 20: 'Question 20'}
+    const questionDict = {1: 'Tell me about yourself', 2: 'Why do you want to work here?', 3: 'Question 3', 4: 'Question 4', 5: 'Question 5', 6: 'Question 6', 7: 'Question 7', 8: 'Question 8', 9: 'Question 9', 10: 'Question 10', 11: 'Question 11', 12: 'Question 12', 13: 'Question 13', 14: 'Question 14', 15: 'Question 15', 16: 'Question 16', 17: 'Question 17', 18: 'Question 18', 19: 'Question 19', 20: 'Question 20'}
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -20,6 +20,7 @@ function InterviewCard(props) {
                 <Card.Body>
                     <Card.Text><h1>Question: {props.questionNum}</h1>
                     <h3>{questionDict[props.questionNum]}</h3>
+                    
                     </Card.Text>
                 </Card.Body>
                 <Card.Footer>
@@ -27,8 +28,16 @@ function InterviewCard(props) {
                 </Card.Footer>
             </Card>
         </div>
-        <Modal show={show} onHide={handleClose} style={{opacity:1}}>
+        <Modal 
+        show={show} 
+        onHide={handleClose} 
+        style={{opacity:1}} 
+        centered
+        dialogClassName="qModal"
+        >
             <QuestionModal 
+                questionNum = {props.questionNum}
+                question = {questionDict[props.questionNum]}
                 handleClose={handleClose}/>
         </Modal>
         
--- src/components/QuestionModal/QuestionModal.jsx
+++ src/components/QuestionModal/QuestionModal.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './QuestionModal.css';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/Collapse';
+
+
 
 function QuestionModal(props) {
+    const [open, setOpen] = useState(false);
 
     return(
-        <>
+        <div className='practiceModal'>
             <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
+                <Modal.Title><h1>Question: {props.questionNum}</h1></Modal.Title>
             </Modal.Header>
-            <Modal.Body>
-                Woohoo, you're reading this text in a modal!
+            <Modal.Body >
+                <p id='question'>
+                    {props.question}
+                </p>
+                <Button 
+                    onClick={() => setOpen(!open)}
+                    aria-controls="tip"
+                    aria-expanded={open}
+                    className="tip-btn"
+                    variant="outline-info"
+                >Show Tip</Button>
+                <Collapse in={open}>
+                    <div className="tip">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum massa massa, efficitur at est ut, iaculis hendrerit ante. Pellentesque convallis varius interdum. Praesent nunc dui, dignissim at nisi sit amet, porta blandit nunc. Nunc molestie ligula odio. Aenean sem nisl, accumsan gravida bibendum id, lobortis vitae lorem. Mauris vitae neque dapibus, elementum neque eu, ullamcorper lectus. Fusce laoreet mattis malesuada.
+                    </div>
+                </Collapse>
             </Modal.Body>
             <Modal.Footer>
-            <Button variant="primary" onClick={props.handleClose}>
+            <Button variant="info" onClick={props.handleClose} style={{fontSize: '20px'}}>
                 Finished!
              </Button>
             </Modal.Footer>
-        </>
+        </div>
     );
 }
 
