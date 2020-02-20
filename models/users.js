ef8615824d0ee6fd565b74f6549e0a19b151f89f
Stanley Sham <stanley.sham94@gmail.com>
2020-02-20 16:03:25 -0500

    adding more models stuff

--- models/users.js
+++ models/users.js
@@ -6,7 +6,8 @@ const SALT_ROUNDS = 6;
 const userSchema = new mongoose.Schema({
     name: String,
     email: {type: String, required: true, lowercase: true, unique: true},
-    password: String
+    password: String,
+    questions: [{type: Schema.Types.Objectid, ref: 'Questions'}]
 }, {
     timestamps: true
 });
