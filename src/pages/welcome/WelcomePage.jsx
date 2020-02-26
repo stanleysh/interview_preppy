7a3b88c9c31bfc2ad422e8ce0d8eaa38fc902802
Stanley Sham <stanley.sham94@gmail.com>
2020-02-26 16:02:10 -0500

    adding atlus connection

--- src/pages/welcome/WelcomePage.jsx
+++ src/pages/welcome/WelcomePage.jsx
@@ -4,8 +4,23 @@ import './WelcomePage.css'
 import Button from 'react-bootstrap/Button';
 
 
-const WelcomePage = () => {
-    
+const WelcomePage = (props) => {
+    if (props.user) {
+        return (
+            <div>
+                <h1 className='welcomeLogo fade-in'>Welcome</h1>
+                <div className="welcome-bar">
+                    <Link to='/demo'><Button variant="outline-info welcome-btn fade-in">Your Questions</Button></Link>
+                    <Link to='/signup'><Button variant="outline-info welcome-btn fade-in">Sign-up</Button></Link>
+                    <Link to='/login'><Button variant="outline-info welcome-btn fade-in">Login</Button></Link>
+                </div>
+                <div className="About">
+                    <h1>About</h1>
+                    <h4>This tool was created to help people get more familiar with interview prep questions, as well as give tips and tricks to help answer certain questions.</h4>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div>
