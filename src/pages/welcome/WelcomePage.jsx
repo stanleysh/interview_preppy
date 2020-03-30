fac70af60984f43bd1f7bb9883f2e4aba1890cba
Stanley Sham <stanley.sham94@gmail.com>
2020-03-30 19:56:22 -0400

    fixed welcome page layout and css

--- src/pages/welcome/WelcomePage.jsx
+++ src/pages/welcome/WelcomePage.jsx
@@ -7,25 +7,21 @@ import Button from 'react-bootstrap/Button';
 const WelcomePage = (props) => {
     if (props.user) {
         return (
-            <div>
+            <div className="welcome-bar-logged-in">
                 <h1 className='welcomeLogo fade-in'>Welcome</h1>
-                <div className="welcome-bar">
-                    <Link to='/demo'><Button variant="outline-info welcome-btn fade-in" className='your-questions'>Your Questions</Button></Link>
-                    <Link to='/'><Button variant="outline-info welcome-btn fade-in" onClick={props.handleLogout}>Logout</Button></Link>
-                </div>
+                <Link to='/demo'><Button variant="outline-info welcome-btn-questions fade-in" className='your-questions'>Your Questions</Button></Link>
+                <Link to='/'><Button variant="outline-info welcome-btn-logout fade-in" onClick={props.handleLogout}>Logout</Button></Link>
             </div>
         )
     }
 
     return (
-        <div>
+        <div className="welcome-bar">
             <h1 className='welcomeLogo fade-in'>Welcome</h1>
-            <div className="welcome-bar">
                 <Link to='/demo'><Button variant="outline-info welcome-btn fade-in">Demo</Button></Link>
                 <Link to='/signup'><Button variant="outline-info welcome-btn fade-in">Sign-up</Button></Link>
                 <Link to='/login'><Button variant="outline-info welcome-btn fade-in">Login</Button></Link>
             </div>
-        </div>
     )
 }
 
