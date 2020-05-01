3a7e2ec0800987b23ab894e4e01bbba2e3981b2e
Stanley Sham <stanley.sham94@gmail.com>
2020-04-30 22:20:00 -0400

    fixed linkedin issue

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -10,10 +10,10 @@ const AboutPage = () => {
             <h4>This tool was created to help people get more familiar with interview prep questions, as well as give tips and tricks to help answer certain questions.</h4>
             <div className="Creators">
                 <Card border="info" style={{width: '27rem', justifySelf: 'center'}}>
-                    <Card.Img variant='top' src="https://media-exp1.licdn.com/dms/image/C5603AQG9AX5M7FF0pQ/profile-displayphoto-shrink_800_800/0?e=1588204800&v=beta&t=tnLUigoDESlC3-6OPXTrcFsC6sOW9ss3IsZz6M11Q5Q" style={{width: '23rem', margin: '5px auto', borderRadius: '15px'}}></Card.Img>
+                    <Card.Img variant='top' src="https://i.imgur.com/lYH8jPD.jpg" style={{width: '23rem', margin: '5px auto', borderRadius: '15px'}}></Card.Img>
                     <Card.Header>
                         <Card.Title style={{fontSize: '20px'}}>Andie He</Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">UI/UX Designer</Card.Subtitle>
+                        <Card.Subtitle className="mb-2 text-muted">Product Designer</Card.Subtitle>
                     </Card.Header>
                     <Card.Body>
                         <Card.Text style={{fontSize: '13px'}}><img src='https://image.flaticon.com/icons/svg/281/281769.svg' alt='gmail logo' style={{width:'20px'}}/> andiehe12@gmail.com</Card.Text>
@@ -22,7 +22,7 @@ const AboutPage = () => {
                     </Card.Body>
                 </Card>
                 <Card border="info" style={{width: '27rem', justifySelf: 'center'}}>
-                    <Card.Img variant='top' src="https://media-exp1.licdn.com/dms/image/C4E03AQHqQSjbOiTFiQ/profile-displayphoto-shrink_200_200/0?e=1588204800&v=beta&t=xPbN_L2Dts1fUJI1MYTzaFrhA5ew-npbIoaHlD6y49I" style={{width: '23rem', margin: '5px auto', borderRadius: '15px'}}></Card.Img>
+                    <Card.Img variant='top' src="https://i.imgur.com/kITwQ11.jpg" style={{width: '23rem', margin: '5px auto', borderRadius: '15px'}}></Card.Img>
                     <Card.Header>
                         <Card.Title style={{fontSize: '20px'}}>Stanley Sham</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Web Developer</Card.Subtitle>
