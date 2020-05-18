b0751b25c3cfbdce1dcde501dcc17ce3a20998d9
Stanley Sham <stanley.sham94@gmail.com>
2020-05-17 20:36:21 -0400

    removed about page stuff

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -9,7 +9,7 @@ const AboutPage = () => {
             <h1>About</h1>
             <h4>This tool was created to help people get more familiar with interview prep questions, as well as give tips and tricks to help answer certain questions.</h4>
             <div className="Creators">
-                <Card border="info" style={{width: '27rem', justifySelf: 'center'}}>
+                {/* <Card border="info" style={{width: '27rem', justifySelf: 'center'}}>
                     <Card.Img variant='top' src="https://i.imgur.com/lYH8jPD.jpg" style={{width: '23rem', margin: '5px auto', borderRadius: '15px'}}></Card.Img>
                     <Card.Header>
                         <Card.Title style={{fontSize: '20px'}}>Andie He</Card.Title>
@@ -20,7 +20,7 @@ const AboutPage = () => {
                         <Card.Link href="https://www.linkedin.com/in/andiehe/" style={{fontSize: '13px'}}><img src="https://1000logos.net/wp-content/uploads/2017/03/LinkedIn-Logo-768x768.png" alt='linked-in logo' style={{width: '20px'}}/> linkedin</Card.Link>
                         <Card.Link href="https://andiehe.com/" style={{fontSize: '13px'}}><img src="https://andiehe.com/wp-content/uploads/2019/10/cropped-logo-32x32.png" alt='andie logo' style={{width:'20px'}}/>Andiehe.com</Card.Link>
                     </Card.Body>
-                </Card>
+                </Card> */}
                 <Card border="info" style={{width: '27rem', justifySelf: 'center'}}>
                     <Card.Img variant='top' src="https://i.imgur.com/kITwQ11.jpg" style={{width: '23rem', margin: '5px auto', borderRadius: '15px'}}></Card.Img>
                     <Card.Header>
